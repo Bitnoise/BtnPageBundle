90e33a62ab7d4a59b8c40f6b3ebf736697728972
Adam Misiorny <adam.misiorny@gmail.com>
2015-03-07 09:15:33 +0100

    extract logic from controller to helper service

--- src/Btn/PageBundle/Controller/PageController.php
+++ src/Btn/PageBundle/Controller/PageController.php
@@ -28,54 +28,20 @@ class PageController extends Controller
             throw new \Exception(sprintf('Invalid input in showAction of "%s"', __CLASS__));
         }
 
-        $backUrl = null;
+        $pageHelper = $this->get('btn_page.helper.page');
+        $twigTpl  = null;
+        $backUrl  = null;
+        $content  = array();
+        $template = null;
 
-        if ($url = $this->get('session')->get('_btn_node')) {
+        if (($url = $this->get('session')->get('_btn_node'))) {
             $backUrl = $this->generateUrl('_btn_node', array('url' => $url));
         }
 
-        $content        = array();
-        $template       = '';
-
-        $template = $page->getTemplate();
-
-        if (!empty($template)) {
-            $content   = @unserialize($page->getContent());
-            $templates = $this->container->getParameter('btn_page.templates');
-            $twigTpl   = isset($templates[$template]['template']) ? $templates[$template]['template'] : null;
-            $fields    = isset($templates[$template]['fields']) ? $templates[$template]['fields'] : null;
-
-            if (is_array($content) && $fields) {
-                foreach ($content as $field => $value) {
-                    if (isset($fields[$field]) && !empty($fields[$field]['type'])) {
-                        $fieldConfig = & $fields[$field];
-                        switch ($fieldConfig['type']) {
-                            case 'btn_media':
-                                $content[$field] = $this->get('btn_media.provider.media')->getRepository()->findOneById($value);
-                                break;
-                            case 'entity':
-                            case 'btn_select2_entity':
-                                $orderBy = null;
-                                if (!empty($fieldConfig['query_builder']['orderby'])) {
-                                    $orderType = !empty($fieldConfig['query_builder']['type']) ?
-                                        $fieldConfig['query_builder']['type'] : 'ASC';
-                                    $orderBy = array($fieldConfig['query_builder']['orderby'] => $orderType);
-                                }
-                                $method = 'findOneById';
-                                if (isset($fieldConfig['multiple']) && true === $fieldConfig['multiple']) {
-                                    $method = 'findById';
-                                }
-                                $content[$field] =
-                                    $this->getDoctrine()
-                                         ->getManager()
-                                         ->getRepository($fieldConfig['class'])->$method($value, $orderBy);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                }
-            }
+        if ($page && $page->getTemplate()) {
+            $template = $page->getTemplate();
+            $content  = $pageHelper->getContent($page);
+            $twigTpl  = $pageHelper->getTemplateTwigTpl($template);
         }
 
         return $this->render($twigTpl, array(
